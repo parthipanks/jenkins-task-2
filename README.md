018b8cef0963b7355758e29b3326f527e5deab6e
parthipanks <parthipanvj8@gmail.com>
2025-09-27 19:50:13 +0530

    Initial commit: add build.sh and README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jenkins Task-2
+Simple repo to test Jenkins build and email notification.
+
+Files:
+- build.sh : prints info to console
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "==== BUILD START ===="
+date
+echo "Hello from Jenkins CI build!"
+echo "Listing repo files:"
+ls -la
+echo "==== BUILD END ===="
