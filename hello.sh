ed7bac9ce363ab0d3b4594621bb331a2db88b372
parthipanks <parthipanvj8@gmail.com>
2025-09-28 19:30:42 +0530

    Fix hello.sh syntax for Jenkins

--- hello.sh
+++ hello.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# hello.sh
+echo "Hello from Jenkins build at $(date)" > output.txt
+echo "Test build at $(date)" >> output.txt
+cat output.txt
 
-echo "Hello from Jenkins build at $(date)"
-echo "Test build at $(date)"
-
-Test webhook at Sun Sep 28 19:27:37 IST 2025
