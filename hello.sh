906fe5c47c8e4794d96be4b2c40c83f33610a8ac
parthipanks <parthipanvj8@gmail.com>
2025-09-28 19:03:15 +0530

    Print output directly in Jenkins console

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello from Jenkins build at $(date)" | tee output.txt
-echo "Test build at $(date)" | tee -a output.txt
-exit 0
+echo "Hello from Jenkins build at $(date)"
+echo "Test build at $(date)"
+
 
