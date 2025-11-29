c58684d7d4312aad2b1cf38a51853377d63831a3
Ubuntu <ubuntu@ip-172-31-68-18.ec2.internal>
2025-11-29 13:39:47 +0000

    Force failure for Jenkins email test

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 echo "======== Jenkins Task-2 Demo ========"
 echo "Date and Time: $(date)"
 echo "Running on host: $(hostname)"
 echo "Current directory: $(pwd)"
 echo "Listing files:"
 ls -la
+
+exit 1
+
