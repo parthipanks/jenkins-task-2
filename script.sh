9789a3fa17e04eb4325113f1e05c185b8c7833e8
Ubuntu <ubuntu@ip-172-31-68-18.ec2.internal>
2025-11-29 10:55:49 +0000

    Add simple Jenkins build script

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "======== Jenkins Task-2 Demo ========"
+echo "Date and Time: $(date)"
+echo "Running on host: $(hostname)"
+echo "Current directory: $(pwd)"
+echo "Listing files:"
+ls -la
